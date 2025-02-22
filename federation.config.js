9dc1b56d2f9deb640f8c4be84ab0178e5836cbc9
zavod008 <zavod1357@mail.ru>
2025-02-22 17:51:56 +0300

    init

---
+++ federation.config.js
@@ -0,0 +1,30 @@
+const { withNativeFederation, shareAll } = require('@angular-architects/native-federation/config');
+
+module.exports = withNativeFederation({
+
+  name: 'remote2',
+
+  exposes: {
+    './Component': './src/app/app.component.ts',
+  },
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+
+  sharedMappings: [
+    '@taiga-ui'
+  ],
+
+  skip: [
+    // 'rxjs/ajax',
+    // 'rxjs/fetch',
+    // 'rxjs/testing',
+    // 'rxjs/webSocket',
+    // Add further packages you don't need at runtime
+  ]
+
+  // Please read our FAQ about sharing libs:
+  // https://shorturl.at/jmzH0
+
+});
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import {TuiDialogService, TuiRoot} from "@taiga-ui/core";
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TuiRoot],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'remote2';
+  private readonly dialogs = inject(TuiDialogService);
+
+  ngOnInit() {
+    this.dialogs.open('Hello!').subscribe();
+  }
 }
